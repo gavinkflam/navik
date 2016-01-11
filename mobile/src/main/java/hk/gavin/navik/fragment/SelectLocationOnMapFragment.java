1b2fc95ec001ded22e5bf8b0ddd41fcaab0505ef
Gavin <me@gavin.hk>
2016-01-11 19:33:47 +0800

    Add marker indicator for center of map on SelectLocationOnMapFragment

--- mobile/src/main/java/hk/gavin/navik/fragment/SelectLocationOnMapFragment.java
+++ mobile/src/main/java/hk/gavin/navik/fragment/SelectLocationOnMapFragment.java
@@ -1,15 +1,22 @@
 package hk.gavin.navik.fragment;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.skobbler.ngx.map.*;
 import hk.gavin.navik.R;
 import hk.gavin.navik.activity.SelectLocationOnMapActivity;
 
 public class SelectLocationOnMapFragment extends Fragment {
+
+    @Bind(R.id.mapHolder) SKMapViewHolder mMapHolder;
+    SKMapSurfaceView mMap;
+    MapHandler mMapHandler = new MapHandler();
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -26,6 +33,145 @@ public class SelectLocationOnMapFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         ButterKnife.bind(this, view);
+        mMapHolder.setMapSurfaceListener(mMapHandler);
+    }
+
+    @Override
+    public void onPause() {
+        mMapHolder.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        mMapHolder.onResume();
+        super.onResume();
+    }
+
+    private class MapHandler implements SKMapSurfaceListener {
+
+        @Override
+        public void onActionPan() {
+
+        }
+
+        @Override
+        public void onActionZoom() {
+
+        }
+
+        @Override
+        public void onSurfaceCreated(SKMapViewHolder skMapViewHolder) {
+            mMap = mMapHolder.getMapSurfaceView();
+            mMap.getMapSettings().setShowBicycleLanes(true);
+            mMap.getMapSettings().setCurrentPositionShown(true);
+        }
+
+        @Override
+        public void onMapRegionChanged(SKCoordinateRegion skCoordinateRegion) {
+
+        }
+
+        @Override
+        public void onMapRegionChangeStarted(SKCoordinateRegion skCoordinateRegion) {
+
+        }
+
+        @Override
+        public void onMapRegionChangeEnded(SKCoordinateRegion skCoordinateRegion) {
+
+        }
+
+        @Override
+        public void onDoubleTap(SKScreenPoint skScreenPoint) {
+
+        }
+
+        @Override
+        public void onSingleTap(SKScreenPoint skScreenPoint) {
+
+        }
+
+        @Override
+        public void onRotateMap() {
+
+        }
+
+        @Override
+        public void onLongPress(SKScreenPoint skScreenPoint) {
+
+        }
+
+        @Override
+        public void onInternetConnectionNeeded() {
+
+        }
+
+        @Override
+        public void onMapActionDown(SKScreenPoint skScreenPoint) {
+
+        }
+
+        @Override
+        public void onMapActionUp(SKScreenPoint skScreenPoint) {
+
+        }
+
+        @Override
+        public void onPOIClusterSelected(SKPOICluster skpoiCluster) {
+
+        }
+
+        @Override
+        public void onMapPOISelected(SKMapPOI skMapPOI) {
+
+        }
+
+        @Override
+        public void onAnnotationSelected(SKAnnotation skAnnotation) {
+
+        }
+
+        @Override
+        public void onCustomPOISelected(SKMapCustomPOI skMapCustomPOI) {
+
+        }
+
+        @Override
+        public void onCompassSelected() {
+
+        }
+
+        @Override
+        public void onCurrentPositionSelected() {
+
+        }
+
+        @Override
+        public void onObjectSelected(int i) {
+
+        }
+
+        @Override
+        public void onInternationalisationCalled(int i) {
+
+        }
+
+        @Override
+        public void onBoundingBoxImageRendered(int i) {
+
+        }
+
+        @Override
+        public void onGLInitializationError(String s) {
+
+        }
+
+        @Override
+        public void onScreenshotReady(Bitmap bitmap) {
+
+        }
 
     }
+
 }
