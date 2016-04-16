8ee68f92d070aeafb13407af6ca7cdd7bdc49d2a
Gavin <me@gavin.hk>
2016-04-16 23:13:29 +0800

    Add message api connection to wear activity

--- wear/src/main/java/hk/gavin/navik/MainActivity.java
+++ wear/src/main/java/hk/gavin/navik/MainActivity.java
@@ -9,8 +9,17 @@ import butterknife.Bind;
 import butterknife.BindColor;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import hk.gavin.navik.contract.WearContract;
+import hk.gavin.navik.core.navigation.NKNavigationState;
+import org.apache.commons.lang3.SerializationUtils;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements GoogleApiClient.ConnectionCallbacks, MessageApi.MessageListener {
+
+    private GoogleApiClient mApiClient;
 
     @Bind(R.id.container) BoxInsetLayout mContainer;
     @Bind(R.id.visualAdvisor) ImageView mVisualAdvisor;
@@ -22,13 +31,6 @@ public class MainActivity extends WearableActivity {
     @BindColor(R.color.colorSafe) int mColorSafe;
     @BindColor(R.color.colorSoon) int mColorSoon;
     @BindColor(R.color.colorImmediate) int mColorImmediate;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-    }
 
     @OnClick(R.id.visualAdvisor)
     void changeBackgroundColor() {
@@ -50,6 +52,37 @@ public class MainActivity extends WearableActivity {
                 mMetersToTurn.setText("300 m");
                 mColorMode = 0;
             }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Connect to Google api client
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(Wearable.API)
+                .build();
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.MessageApi.addListener(mApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Do nothing
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        if (messageEvent.getPath().equalsIgnoreCase(WearContract.Path.NAVIGATION_STATE)) {
+            NKNavigationState state = SerializationUtils.deserialize(messageEvent.getData());
         }
     }
 }
