8af71809d76f40e65701533a38bda434ac88cf13
Gavin <me@gavin.hk>
2016-04-20 01:56:26 +0800

    Remove context from presenter

--- mobile/src/main/java/hk/gavin/navik/ui/presenter/AbstractPresenter.java
+++ mobile/src/main/java/hk/gavin/navik/ui/presenter/AbstractPresenter.java
@@ -1,21 +1,12 @@
 package hk.gavin.navik.ui.presenter;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import butterknife.ButterKnife;
-import com.google.common.base.Optional;
-import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Accessors(prefix = "m")
 public abstract class AbstractPresenter {
-
-    @Getter private Optional<Context> mContext = Optional.absent();
-
-    public void setContext(Context context) {
-        mContext = Optional.of(context);
-    }
 
     public void onCreate() {
         // Do nothing
